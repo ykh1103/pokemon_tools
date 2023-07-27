d55e78ee4f6021e9cc481e315a21ba5b3d439a96
ykh1103 <136175600+ykh1103@users.noreply.github.com>
2023-07-27 14:23:39 +0900

    Update decks.py

--- web/applications/decks.py
+++ web/applications/decks.py
@@ -1,8 +1,8 @@
-from ..models import DeckSets
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .execute import get_deck_pokemon_by_deck_id
-import datetime
+from .repostitory import save_deck_sets
+from .repostitory import delete_deck_sets
 
 
 # DeckSetsにポケモンを登録
@@ -23,8 +23,7 @@ def register_deck(request):
         msg = "デッキは６匹までです"
         return redirect(f"/?deck_id={requests['deck_id']}&criteria={requests['criteria']}&msg={msg}")
     # 登録
-    DeckSets(deck_id=requests["deck_id"], pokemon_id=requests["pokemon_id"],
-             created_date=datetime.datetime.now()).save()
+    save_deck_sets(requests["deck_id"], requests["pokemon_id"])
     print(f"id：{requests['pokemon_id']}が登録されました")
     return redirect(f"/?deck_id={requests['deck_id']}&criteria={requests['criteria']}")
 
@@ -47,6 +46,6 @@ def delete_deck(request):
         msg = "デッキにポケモンがいません"
         return redirect(f"/?deck_id={requests['deck_id']}&criteria={requests['criteria']}&msg={msg}")
     # 削除
-    DeckSets.objects.filter(id=requests["deck_set_id"]).delete()
+    delete_deck_sets(requests["deck_set_id"])
     print(f"id：{requests['deck_set_id']}が削除されました")
     return redirect(f"/?deck_id={requests['deck_id']}&criteria={requests['criteria']}")
