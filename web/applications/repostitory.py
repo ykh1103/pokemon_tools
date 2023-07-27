64279d1f69252270d34b77ea6d6b330edc41c889
ykh1103 <136175600+ykh1103@users.noreply.github.com>
2023-07-27 14:25:16 +0900

    Update repostitory.py

--- web/applications/repostitory.py
+++ web/applications/repostitory.py
@@ -4,8 +4,10 @@ from ..models import Skills
 from ..models import Pokemons
 from ..models import DeckSets
 from ..models import SkillSets
+import datetime
 
 
+# 格納場所
 # わざの情報の格納場所
 class SkillView:
     id = None
@@ -73,3 +75,27 @@ class SkillPokemonView:
         self.skill_id = skillSet.skill_id
         self.skill_name = skills.name
         self.pokemon_view = pokemon_view
+
+
+# データベース操作
+# DeckSetsへ保存
+def save_deck_sets(deck_id, pokemon_id):
+    return DeckSets(deck_id=deck_id, pokemon_id=pokemon_id,
+                    created_date=datetime.datetime.now(), created_by="kawano").save()
+
+
+# DeckSetから削除
+def delete_deck_sets(deck_set_id):
+    return DeckSets.objects.filter(id=deck_set_id).delete()
+
+
+# SkillSetsへ保存
+def save_skill_sets(deck_set_id, skill_id):
+    SkillSets(deck_set_id=deck_set_id, skill_id=skill_id,
+              created_date=datetime.datetime.now(), created_by="kawano").save()
+
+
+# SkillSetsから削除
+def delete_skill_sets(skill_id, deck_set_id):
+    SkillSets.objects.filter(skill_id=skill_id, deck_set_id=deck_set_id).delete()
+
